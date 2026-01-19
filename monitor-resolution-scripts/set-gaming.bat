f2b6a8f276c5e8e3862520c93d73a4ad2661f488
Graham McGregor <graham.mcgregor04+github@gmail.com>
2026-01-19 00:19:39 -0500

    flip monitor to be portrait

--- monitor-resolution-scripts/set-gaming.bat
+++ monitor-resolution-scripts/set-gaming.bat
@@ -1,17 +1,18 @@
 @echo off
-:: Gaming Profile: Monitor 2 only at 4K with 200% scaling
-:: Monitor 2 (left): 3840x2160 @ 200%
-:: Monitor 1 (right): Disabled
+:: Gaming Profile: Monitor 1 only at 4K with 200% scaling
+:: Monitor 1 (left): 3840x2160 @ 200%
+:: Monitor 2 (right): Disabled
 
 set TOOL=%~dp0multimonitortool-x64\MultiMonitorTool.exe
 
-:: Disable Monitor 1 (right)
-"%TOOL%" /disable \\.\DISPLAY1
+:: Disable Monitor 2 (right)
+"%TOOL%" /disable \\.\DISPLAY2
 
-:: Set Monitor 2 to 4K
-"%TOOL%" /SetMonitors "Name=\\.\DISPLAY2 Width=3840 Height=2160"
+:: Set Monitor 1 to 4K landscape
+"%TOOL%" /SetMonitors "Name=\\.\DISPLAY1 Width=3840 Height=2160"
+"%TOOL%" /SetOrientation \\.\DISPLAY1 0
 
-:: Set Monitor 2 scaling to 200%
-"%TOOL%" /SetScale \\.\DISPLAY2 200
+:: Set Monitor 1 scaling to 200%
+"%TOOL%" /SetScale \\.\DISPLAY1 200
 
-echo Gaming profile applied: Monitor 2 at 4K @ 200%%, Monitor 1 disabled
+echo Gaming profile applied: Monitor 1 at 4K @ 200%%, Monitor 2 disabled
--- monitor-resolution-scripts/set-streaming.bat
+++ monitor-resolution-scripts/set-streaming.bat
@@ -1,21 +1,23 @@
 @echo off
 :: Streaming Profile: Both monitors at 1440p
-:: Monitor 2 (left): 2560x1440 @ 150%
-:: Monitor 1 (right): 2560x1440 @ 100%
+:: Monitor 1 (left): 2560x1440 @ 150%
+:: Monitor 2 (right): 1440x2560 @ 100% (portrait)
 
 set TOOL=%~dp0multimonitortool-x64\MultiMonitorTool.exe
 
-:: Ensure Monitor 1 is enabled
-"%TOOL%" /enable \\.\DISPLAY1
+:: Ensure Monitor 2 is enabled
+"%TOOL%" /enable \\.\DISPLAY2
 
-:: Set Monitor 2 to 1440p
-"%TOOL%" /SetMonitors "Name=\\.\DISPLAY2 Width=2560 Height=1440"
-
-:: Set Monitor 1 to 1440p
+:: Set Monitor 1 to 1440p landscape
 "%TOOL%" /SetMonitors "Name=\\.\DISPLAY1 Width=2560 Height=1440"
+"%TOOL%" /SetOrientation \\.\DISPLAY1 0
+
+:: Set Monitor 2 to 1440p portrait
+"%TOOL%" /SetMonitors "Name=\\.\DISPLAY2 Width=2560 Height=1440"
+"%TOOL%" /SetOrientation \\.\DISPLAY2 90
 
 :: Set scaling
-"%TOOL%" /SetScale \\.\DISPLAY2 150
-"%TOOL%" /SetScale \\.\DISPLAY1 100
+"%TOOL%" /SetScale \\.\DISPLAY1 150
+"%TOOL%" /SetScale \\.\DISPLAY2 100
 
 echo Streaming profile applied: Both monitors at 1440p (150%% / 100%%)
